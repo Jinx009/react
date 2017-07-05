7e654b445da03b39f94457b19831795ffde3dc65
Jinx <jinxlovejinx@vip.qq.com>
2017-07-05 14:45:08 +0800

    react

---
+++ README.md
@@ -0,0 +1,8 @@
+#react
+reactjs study note
+
+cd my-app
+
+npm start
+
+http://locahost:3000
---
+++ my-app/public/helloworld_react.js
@@ -0,0 +1,89 @@
+/**
+ * Created by jinx on 7/5/17.
+ */
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
+
+var a = 3;
+ReactDOM.render(
+    <div>
+        <h1>{a == 1 ? 'True!' : 'False'}</h1>
+    </div>
+    ,
+    document.getElementById('example1')
+);
+
+
+ReactDOM.render(
+    <div>
+        <h1>{1 + 1}</h1>
+    </div>
+    ,
+    document.getElementById('example2')
+);
+
+
+var myStyle = {
+    fontSize: 22,
+    color: '#FF0000'
+};
+ReactDOM.render(
+    <h1 style={myStyle}>Just test</h1>,
+    document.getElementById('example3')
+);
+
+
+var MyComponent = React.createClass({
+    render: function () {
+        return <h1>Hello World！</h1>;
+    }
+});
+var myElement = <MyComponent someProperty={true}/>;
+ReactDOM.render(myElement, document.getElementById('example4'));
+
+
+var HelloMessage = React.createClass({
+    render: function () {
+        return <h1>Hello {this.props.name}</h1>;
+    }
+});
+ReactDOM.render(
+    <HelloMessage name="Runoob"/>,
+    document.getElementById('example5')
+);
+
+
+var WebSite = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Name name={this.props.name}/>
+                <Link site={this.props.site}/>
+            </div>
+        );
+    }
+});
+var Name = React.createClass({
+    render: function () {
+        return (
+            <h1>{this.props.name}</h1>
+        );
+    }
+});
+var Link = React.createClass({
+    render: function () {
+        return (
+            <a href={this.props.site}>
+                {this.props.site}
+            </a>
+        );
+    }
+});
+ReactDOM.render(
+    <WebSite name="Jinx's test " site="jinx"/>,
+    document.getElementById('example6')
+);
+
+
